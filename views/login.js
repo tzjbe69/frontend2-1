78607c53e8907b9473bbbdb9615ec1c1135277a5
Siegfried Arnold Aczberger <siegfriedarnold.aczberger@gameloft.com>
2018-01-20 13:19:30 +0200

    Updated errors for failed login

--- views/login.js
+++ views/login.js
@@ -14,57 +14,11 @@ window.onload = function() {
         userLogin.username = usernameInput.value;
         userLogin.password = passwordInput.value;
 
-        // -------- TO BE TESTED --------
-
-        // validateEmptyInputs(usernameInput, passwordInput);
-        // validateInput(userLogin.username, usernameInput);
-        // validateInput(userLogin.password, passwordInput);
-
-        // function checkValidation() {
-        //     if (validateEmptyInputs(usernameInput, passwordInput) && validateInput(userLogin.username, usernameInput) && validateInput(userLogin.password, passwordInput)) {
-        //         return true;
-        //     }
-        //     else {
-        //         return false;
-        //     }
-        // };
-
-        // function checkValidation() {
-        //     if(!validateEmptyInputs(usernameInput, passwordInput)) {
-        //         if (!validateInput(userLogin.username, usernameInput)) {
-        //             validateInput(userLogin.password, passwordInput);
-        //         }
-        //         return true;
-        //     }
-        //     else {
-        //         return false;
-        //     }
-        // }
-
-        // checkValidation();
-        
-        // if(checkValidation()) {
-        //     userLogin.login()
-        //     .then(response => {
-        //         if(response.authenticated) {
-        //             document.cookie = 'accessCookie=' + response.accessToken;
-        //             loginNotif.innerHTML = "<h4>Login Successful, young Padawan " + userLogin.username + "</h4>";
-        //             loginNotif.style.backgroundColor = "#07b001";
-        //             setTimeout(() => {document.location.href = "home.html"}, 3000);
-        //         }
-        //         else {
-        //             loginNotif.innerHTML = "<h4>Your credentials are incorrect.</h4>";
-        //             loginNotif.style.backgroundColor = "#ba1a14";
-        //         }
-        //     });
-        // }    
-
-        // ----------
-
         userLogin.login()
             .then(response => {
             	if(response.authenticated) {
                 	document.cookie = 'accessCookie=' + response.accessToken;
+                    document.cookie = "username=" + userLogin.username;
                     loginNotif.innerHTML = "<h4>Login Successful, young Padawan " + userLogin.username + "</h4>";
                     loginNotif.style.backgroundColor = "#07b001";
                 	setTimeout(() => {document.location.href = "home.html"}, 3000);
@@ -78,36 +32,3 @@ window.onload = function() {
     });
 }
 
-// ADD SYMBOL VALIDATION FOR USERNAME!
-
-function validateInput(value, inputType) {
-    const validationError = document.querySelector('.validation-error');
-    if (value === "") {
-        inputType.style.border = '2px solid red';
-        validationError.innerHTML = "<h5>You can not have blank inputs.</h5>";
-        return false;
-    } else if (value.length < 6) {
-        inputType.style.border = '2px solid red';
-        validationError.innerHTML = "<h5>Your " + inputType.name + " must be at least 6 characters long.</h5>";
-        return false;
-    } else if (value !== "" && value > 5) {
-        inputType.style.border = '1px solid #6b6a69';
-        validationError.innerHTML = "<h5></h5>";
-        return true;
-    }
-}
-
-function validateEmptyInputs(input1, input2) {
-    const validationError = document.querySelector('.validation-error');
-    if (input1.value === "" && input2.value === "") {
-        input1.style.border = '2px solid red';
-        input2.style.border = '2px solid red';
-        validationError.innerHTML = "<h5>You can not have blank inputs.</h5>";
-        return false;
-    } else {
-        input1.style.border = '1px solid #6b6a69';
-        input2.style.border = '1px solid #6b6a69';
-        validationError.innerHTML = "<h5></h5>";
-        return true;
-    }
-}
